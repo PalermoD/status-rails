50112993791abf20ef8f3b77831c230c7d533120
PalermoD <palermo.deschamps@gmail.com>
2018-06-18 14:29:31 -0400

    users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
         if @user.save
             log_in @user
             flash[:success] = "Welcome to Status"
-            redirect_to @user
+            redirect_to root_url
         else
             render 'new'
         end
