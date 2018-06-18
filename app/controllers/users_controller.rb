514e9fc5318172f8f83a6bd5f2c310eea2742291
PalermoD <palermo.deschamps@gmail.com>
2018-06-18 14:18:21 -0400

    login copy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             log_in @user
-            flash[:success] = "Welcome to the Sample App!"
+            flash[:success] = "Welcome to the Status"
             redirect_to root_url
         else
             render 'new'
