811cddea84804b8ab3485b471167698ebbd2cb77
PalermoD <palermo.deschamps@gmail.com>
2018-06-24 15:14:08 -0400

    add raw

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -20,7 +20,7 @@ class MicropostsController < ApplicationController
   def destroy
     @micropost.destroy
     flash[:success] = "Post deleted"
-    redirect_to request.referrer || root_url
+    redirect_to root_url
   end
 
   def show
