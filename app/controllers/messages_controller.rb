0221354c4f2166e3fc6d2779adab99ebd483f349
PalermoD <palermo.deschamps@gmail.com>
2018-06-25 22:42:37 -0400

    bug

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -28,9 +28,7 @@ class MessagesController < ApplicationController
     def create
         @message = @conversation.messages.new(message_params)
         if @message.save
-            ActionCable.server.broadcast 'messages',
-                message: @message.body,
-                user: @message.user.name
+
 
             redirect_to conversation_messages_path(@conversation)
         end
