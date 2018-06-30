5a77aaf74c05dbd4b848bbf0c24a29a08649b940
PalermoD <palermo.deschamps@gmail.com>
2018-06-29 20:44:06 -0400

    update message style

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,15 +4,15 @@ class MessagesController < ApplicationController
     end
 
     def index
-        @messages = @conversation.messages
-        if @messages.length > 10
-            @over_ten = true
-            @messages = @messages[-10..-1]
-        end
-        if params[:m]
-            @over_ten = false
-            @messages = @conversation.messages
-        end
+        @messages = @conversation.messages.order(created_at: :desc).paginate(page: params[:page])
+        # if @messages.length > 10
+        #     @over_ten = true
+        #     @messages = @messages[-10..-1].paginate(page: params[:page])
+        # end
+        # if params[:m]
+        #     @over_ten = false
+        #     @messages = @conversation.messages.paginate(page: params[:page])
+        # end
         if @messages.last
             if @messages.last.user_id != current_user.id
                 @messages.last.read = true;
