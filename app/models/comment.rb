0af481c6719ee3b2bae18221cf2081c92e4a456f
PalermoD <palermo.deschamps@gmail.com>
2018-08-13 20:25:37 -0400

    remove validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :micropost
-  validates :content, presence: true
-  default_scope -> { order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
+  # validates :content, presence: true
+  default_scope -> { order(created_at: :desc) }
+
 end
