836fe0a5009326202246021ebd6f59415a3f2019
PalermoD <palermo.deschamps@gmail.com>
2018-11-08 03:55:48 -0500

    rank tags

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -7,6 +7,7 @@ class HashtagsController < ApplicationController
   def show
     @hashtag = SimpleHashtag::Hashtag.find_by_name(params[:hashtag])
     @hashtagged = @hashtag.hashtaggables if @hashtag
+    @hashtagged = @hashtagged.sort_by! {|h| -h.cached_votes_score}
   end
 
 end
